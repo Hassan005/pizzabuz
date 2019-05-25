41b6ee741de2a9cdce4e7df230dd86dff8244a34
Hi there <hassanasghar666@gmail.com>
2019-05-26 04:24:20 +0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 //var express=require('express');
 const debug = require("debug")("node-angular");
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/'));
 
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/pizzabuz/index.html'));
+});
 var app = require("./backend/app");
 port=3000
 
